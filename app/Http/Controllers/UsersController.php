547b447c29dcf8be0583c5cdc92426c8f548ad8d
xuelinjie <632829328@163.com>
2020-11-16 18:16:20 +0800

    '登录功能'

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,7 +18,7 @@ class UsersController extends Controller{
     public function store(Request $request){
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
-            'email' => 'required|email|unique|max:255',
+            'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6',
         ]);
 
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+//name 里的名字 是给页面使用的
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
@@ -26,3 +27,8 @@ Route::get('signup', 'UsersController@create')->name('signup');
 
 Route::resource('users', 'UsersController');
 // Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+
+//登录
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
