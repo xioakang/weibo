e8183ef758b0a7c9871e7cefb3108d7c6775eb44
xuelinjie <632829328@163.com>
2020-11-16 14:59:49 +0800

    '注册页面错误提示'

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,7 +10,17 @@ class UsersController extends Controller{
         return view("users.create");
     }
 
-    public function show(User $user){
+    public function show(User $user)
+    {
         return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique|max:255',
+            'password' => 'required|confirmed|min:6',
+        ]);
+        return;
     }
 }
